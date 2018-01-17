d15e7b4942807b3314ffb9cceaf23bb7668a006d
Ken Kite <kenneth.kite@gmail.com>
2018-01-17 13:31:40 -0500

    1.0

--- 04_WorkingWithDataSources/03_SQLAndDatabases_Intermediate_PostgreSQL/03_DatabaseNormalizationAndRelations/06_JoinTable.sql
+++
@@ -1,5 +0,0 @@
-CREATE table movies_actors (
-id INTEGER PRIMARY KEY,
-movie_id INTEGER REFERENCES movies(id),
-actor_id INTEGER REFERENCES actors(id) 
-);
--- 04_WorkingWithDataSources/03_SQLAndDatabases_Intermediate_PostgreSQL/06_UsingPostgreSQL/03_Psycopg2.py
+++
@@ -1,5 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(dbname="dq", user="postgres")
-cur = conn.cursor()
-print(cur)
-conn.close()
