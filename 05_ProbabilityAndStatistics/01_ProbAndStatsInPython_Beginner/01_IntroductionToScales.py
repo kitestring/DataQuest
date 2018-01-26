05dde3e0d6bc1ee227d324d9b83fd0b2ee80841e
Ken Kite <kenneth.kite@gmail.com>
2018-01-26 15:49:54 -0500

    Started Stats

--- 05_ProbabilityAndStatistics/01_ProbAndStatsInPython_Beginner/01_IntroductionToScales.py
+++ 05_ProbabilityAndStatistics/01_ProbAndStatsInPython_Beginner/01_IntroductionToScales.py
@@ -1,1 +1,12 @@
-print('test')
+# Compute the mean of car_speeds, and assign the result to mean_car_speed.
+# Compute the mean of earthquake_intensities, and assign the result to mean_earthquake_intensities. 
+#     Note that this value will not be meaningful, because we shouldn't average values on a logarithmic scale this way.
+
+car_speeds = [10,20,30,50,20]
+earthquake_intensities = [2,7,4,5,8]
+
+mean_car_speed = sum(car_speeds) / len(car_speeds)
+print('Mean Car Speed:', mean_car_speed)
+mean_earthquake_intensities = sum(earthquake_intensities) / len(earthquake_intensities)
+print('Mean Earthquake Intensities:', mean_earthquake_intensities)
+print("Note that this value i meaningful, because we shouldn't average values on a logarithmic scale this way.")
---
+++ 05_ProbabilityAndStatistics/01_ProbAndStatsInPython_Beginner/02_DiscreteAndContinuousScales.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+day_numbers = [1,2,3,4,5,6,7]
+snail_crawl_length = [.5,2,5,10,1,.25,4]
+cars_in_parking_lot = [5,6,4,2,1,7,8]
+
+x_data = day_numbers
+y_data = [snail_crawl_length, cars_in_parking_lot]
+labels = ['snail_crawl_length', 'cars_in_parking_lot']
+
+color_codes = ['red', 'blue', 'green', 'orange', 'black']
+
+fig = plt.figure(figsize=(10, 6)) 
+
+for plot in range(0,2):
+    
+    plt.plot(x_data, y_data[plot], color=color_codes[plot], label=labels[plot])
+  
+
+plt.legend(loc='upper left')
+plt.xlabel('day_numbers')
+plt.ylabel('Unrelated Shit')
+plt.title('Seeing the difference between linear and logrithmic data sets')    
+plt.show()
