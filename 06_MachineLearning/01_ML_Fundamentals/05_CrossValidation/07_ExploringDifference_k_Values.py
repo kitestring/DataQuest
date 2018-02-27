092aec28991a2364ce2f06f82acffc7cd597dba2
Ken Kite <kenneth.kite@gmail.com>
2018-02-27 10:47:23 -0500

    Cross Validation, Done

---
+++ 06_MachineLearning/01_ML_Fundamentals/05_CrossValidation/07_ExploringDifference_k_Values.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.neighbors.regression import KNeighborsRegressor
+
+# reads and cleans DataFrame
+dc_listings = pd.read_csv("dc_airbnb.csv")
+dc_listings.rename(columns=lambda col_header: col_header.strip(), inplace=True)
+stripped_commas = dc_listings['price'].str.replace(',', '')
+stripped_dollars = stripped_commas.str.replace('$', '')
+dc_listings['price'] = stripped_dollars.astype('float')
+
+num_folds = [3, 5, 7, 9, 10, 11, 13, 15, 17, 19, 21, 23]
+
+for fold in num_folds:
+    kf = KFold(fold, shuffle=True, random_state=1)
+    model = KNeighborsRegressor()
+    mses = cross_val_score(model, dc_listings[["accommodates"]], dc_listings["price"], scoring="neg_mean_squared_error", cv=kf)
+    rmses = np.sqrt(np.absolute(mses))
+    avg_rmse = np.mean(rmses)
+    std_rmse = np.std(rmses)
+    print(str(fold), "folds: ", "avg RMSE: ", str(avg_rmse), "std RMSE: ", str(std_rmse))
+    
