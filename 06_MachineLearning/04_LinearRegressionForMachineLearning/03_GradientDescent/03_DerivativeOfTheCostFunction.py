f3b7c4653af9ea46e7399ccf1717e073c651788e
Ken Kite <kenneth.kite@gmail.com>
2018-03-21 10:18:18 -0400

    Gradient Descent

--- 06_MachineLearning/04_LinearRegressionForMachineLearning/03_GradientDescent/03_DerivativeOfTheCostFunction.py
+++ 06_MachineLearning/04_LinearRegressionForMachineLearning/03_GradientDescent/03_DerivativeOfTheCostFunction.py
@@ -1,8 +1,16 @@
+# page 36 & 37 in the hand written notes for context
+# also review the MachineLearning.doc
+
 import pandas as pd
 
 def derivative(a1, xi_list, yi_list):
-    # Modify this function.
-    return 0
+    
+    der = []
+    n = len(xi_list)
+    for i in range(n):
+        der.append(xi_list[i] * ((a1 * xi_list[i]) - yi_list[i]))
+    
+    return (2/n) * sum(der)
 
 def gradient_descent(xi_list, yi_list, max_iterations, alpha, a1_initial):
     a1_list = [a1_initial]
@@ -18,6 +26,8 @@ data = pd.read_csv('AmesHousing.txt', delimiter='\t')
 train = data.iloc[:1460]
 test = data.iloc[1460:]
 
-# Uncomment when ready.
-# param_iterations = gradient_descent(train['Gr Liv Area'], train['SalePrice'], 20, .0000003, 150)
-# final_param = param_iterations[-1]
+param_iterations = gradient_descent(train['Gr Liv Area'], train['SalePrice'], 20, .0000003, 150)
+final_param = param_iterations[-1]
+print('final_param:', final_param)
+
+# final_param: 120.142191472
