d8b637c3fe093056f66d9aab0ce523e7ca35cdf8
Ken <kitestring@users.noreply.github.com>
2018-04-06 16:23:26 -0400

    Gradient descent, 1/8 Done

---
+++ 06_MachineLearning/05_ML_Intermediate/07_GraidentDescent/01_IntroductionToTheData.py
@@ -0,0 +1,18 @@
+import pandas
+import matplotlib.pyplot as plt
+import os
+
+# Read data from csv
+pga = pandas.read_csv("pga.csv")
+
+# Normalize the data
+pga.distance = (pga.distance - pga.distance.mean()) / pga.distance.std()
+pga.accuracy = (pga.accuracy - pga.accuracy.mean()) / pga.accuracy.std()
+print(pga.head())
+
+plt.scatter(pga.distance, pga.accuracy)
+plt.xlabel('normalized distance')
+plt.ylabel('normalized accuracy')
+plot_file_name = os.path.splitext(os.path.basename(__file__))[0]+'_fig1.png'
+plt.savefig(plot_file_name, bbox_inches='tight')
+plt.show()
